074cb8b0e841dbbe0fd943424473749cd477d62a
oknoorap <oknoorap@gmail.com>
2015-04-09 20:36:33 +0700

    fix htaccess

--- update.sh
+++ update.sh
@@ -24,6 +24,21 @@ mv augencont/* ./
 rm augencont -rf
 
 #--------------------------
+# nano .htaccess
+#--------------------------
+cat << EOFTEST1 > ./.htaccess
+<IfModule mod_rewrite.c>
+    RewriteEngine On
+
+    RewriteRule ^sitemap([0-9]{0,3})?\.xml(\.gz)?$ sitemap.php?offset=$1&format=$2 [QSA,L]
+    RewriteCond %{REQUEST_FILENAME} !-f
+    RewriteCond %{REQUEST_FILENAME} !-d
+    RewriteRule ^(.*)$ index.php?\$1 [L,QSA]
+</IfModule>
+EOFTEST1
+sudo service apache2 restart
+
+#--------------------------
 # Fix Permission
 #--------------------------
 find /var/www/ -type d -exec chmod 755 {} \;
