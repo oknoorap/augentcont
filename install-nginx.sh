7e526cadda188812916e87cd43f99f2d9a67b904
oknoorap <oknoorap@gmail.com>
2015-05-08 02:10:05 +0700

    Fix nginx installation

--- install-nginx.sh
+++ install-nginx.sh
@@ -171,15 +171,15 @@ server {
 		auth_basic "Admin Login";
 		auth_basic_user_file /etc/nginx/pma_pass;
 		index index.php index.html index.htm;
-		location ~ ^/phpmyadmin/(.+\.php)$ {
-			try_files $uri =404;
+		location ~ ^/phpmyadmin/(.+\\.php)\$ {
+			try_files \$uri =404;
 			root /usr/share/;
 			fastcgi_pass unix:/var/run/php5-fpm.sock;
 			fastcgi_index index.php;
-			fastcgi_param SCRIPT_FILENAME $document_root$fastcgi_script_name;
+			fastcgi_param SCRIPT_FILENAME \$document_root\$fastcgi_script_name;
 			include fastcgi_params;
 		}
-		location ~* ^/phpmyadmin/(.+\.(jpg|jpeg|gif|css|png|js|ico|html|xml|txt))$ {
+		location ~* ^/phpmyadmin/(.+\\.(jpg|jpeg|gif|css|png|js|ico|html|xml|txt))\$ {
 			root /usr/share/;
 		}
 	}
